b6fd8db8f52a6599893e6d64113479b1a82f9abb
NguyenNgocQuynhNhi <quynhnhi7bpct@gmail.com>
2024-04-28 16:36:46 +0700

    Update queue.c
    Nhi uploads code in queue.c

--- src/queue.c
+++ src/queue.c
@@ -9,12 +9,22 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if (q->size == MAX_QUEUE_SIZE) exit(1);
+        q->proc[q->size] = proc; // Thêm tiến trình vào hàng đợi
+        q->size++;               // Cập nhật kích thước của hàng đợi
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+        if (q == NULL || q->size == 0) return NULL;
+        struct pcb_t *proc = q->proc[0];
+        for (int i = 0; i < q->size-1; i++) {
+          q->proc[i] = q->proc[i+1];
+        }
+        q->proc[q->size-1] = NULL;
+        q->size--;
+	return proc;
 }
 
