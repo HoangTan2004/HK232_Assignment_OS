30513c4d360893ec66f6050b7a0da74e40d142da
lnun187 <167327421+lnun187@users.noreply.github.com>
2024-05-09 19:55:52 +0700

    Update sched.c

--- btlos/src/sched.c
+++ btlos/src/sched.c
@@ -8,36 +8,27 @@
 static struct queue_t ready_queue;
 static struct queue_t run_queue;
 static pthread_mutex_t queue_lock;
-
-#ifdef MLQ_SCHED
 static struct queue_t mlq_ready_queue[MAX_PRIO];
 static int curr_prio = 0;
 static int slot_left = MAX_PRIO;
-#endif
 
 int queue_empty(void) {
-#ifdef MLQ_SCHED
 	unsigned long prio;
 	for (prio = 0; prio < MAX_PRIO; prio++)
 		if(!empty(&mlq_ready_queue[prio])) 
 			return -1;
-#endif
 	return (empty(&ready_queue) && empty(&run_queue));
 }
 
 void init_scheduler(void) {
-#ifdef MLQ_SCHED
     int i;
-
 	for (i = 0; i < MAX_PRIO; i ++)
 		mlq_ready_queue[i].size = 0;
-#endif
 	ready_queue.size = 0;
 	run_queue.size = 0;
 	pthread_mutex_init(&queue_lock, NULL);
 }
 
-#ifdef MLQ_SCHED
 /* 
  *  Stateful design for routine calling
  *  based on the priority and our MLQ policy
@@ -50,9 +41,13 @@ struct pcb_t * get_mlq_proc(void) {
 	 * Remember to use lock to protect the queue.
 	 * */
 	pthread_mutex_lock(&queue_lock);
-	if(slot_left == 0) curr_prio = 0;
+	if(slot_left == 0 || empty(&mlq_ready_queue[curr_prio])) {
+		curr_prio = 0;
+		slot_left = MAX_PRIO - curr_prio;
+	}
 	for (int i = 0; i < MAX_PRIO; i++) {
         if (slot_left == 0 || empty(&mlq_ready_queue[curr_prio])) {
+			//if(curr_prio < 10) printf("ready queue prio %d empty\n", curr_prio);
             curr_prio = (curr_prio + 1) % MAX_PRIO;
             slot_left = MAX_PRIO - curr_prio;
         }
@@ -62,7 +57,6 @@ struct pcb_t * get_mlq_proc(void) {
             break;
         }
 	}
-    
 	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
@@ -88,37 +82,8 @@ void put_proc(struct pcb_t * proc) {
 }
 
 void add_proc(struct pcb_t * proc) {
+	//printf("vao add proc\n");
 	return add_mlq_proc(proc);
 }
-#else
-struct pcb_t *get_proc(void)
-
-{
-	struct pcb_t *proc = NULL;
-	/* TODO: get a process from [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 * */
-	pthread_mutex_lock(&queue_lock);
-	if (!empty(&ready_queue))
-	{
-		proc = dequeue(&ready_queue);
-	}
-	pthread_mutex_unlock(&queue_lock);
-	return proc;
-
-}
-
-void put_proc(struct pcb_t * proc) {
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
-}
-
-void add_proc(struct pcb_t * proc) {
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);	
-}
-#endif
 
 
